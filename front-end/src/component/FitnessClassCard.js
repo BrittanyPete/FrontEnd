8925b46e30c11fbcc3ecc8112add2060d259d07f
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-01-31 15:30:05 -0800

    added card

---
+++ front-end/src/component/FitnessClassCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const FitnessClassCard = (props) => {
+ 
+  const { person } = props;
+  return (
+    <div>
+      <div>{person.name}</div>
+      <div>{person.email}</div>
+      <div>{person.role}</div>
+      <Link to={`/edit/${person.id}`}>Edit</Link>
+    </div>
+  );
+};
+
+export default FitnessClassCard;
