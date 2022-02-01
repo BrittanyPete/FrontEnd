21f466c8ed069687650391ad8ff4d4875112f20b
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-01 10:44:26 -0500

    built orientation slides but still need arrow buttons to work

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -20,6 +20,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Orientation />
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Login />} />
--- front-end/src/component/Orientation.js
+++ front-end/src/component/Orientation.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+import './Orientation.css';
+
 const Orientation = () => {
+
+    const slides = [1, 2, 3, 4, 5];
+
     return(
-        <div></div>
+
+        <div className='orientation-container'>
+            <div className='title'>Welcome to Anytime Fitness!</div>
+            <button className='back-arrow'> ← </button>
+            <div className='slides'>
+                {
+                    slides.map((slides, i) => {
+                        return(
+                            <div className='slide-card'> This is it
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <button className='forward-arrow'> →  </button>
+
+        </div>
+
     )
 }
 
