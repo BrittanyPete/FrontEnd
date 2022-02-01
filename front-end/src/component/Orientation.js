e96cc011101c03f3a04a3d2607c73805878dcbb4
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-01 12:35:01 -0500

    should be the same as my last push but just in case

--- front-end/src/component/Orientation.js
+++ front-end/src/component/Orientation.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Orientation.css';
 
+
+
 const Orientation = () => {
 
+    // have to build out the actual slide data
+    // these are placeholder slides
     const slides = [1, 2, 3, 4, 5];
+
+
+    const handleClickLeft = () => {
+        
+    }
+
+    const handleClickRight = () => {
+       
+
+    }
 
     return(
 
         <div className='orientation-container'>
             <div className='title'>Welcome to Anytime Fitness!</div>
-            <button className='back-arrow'> ← </button>
-            <div className='slides'>
-                {
-                    slides.map((slides, i) => {
-                        return(
-                            <div className='slide-card'> This is it
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <button className='forward-arrow'> →  </button>
+            <button className='back-arrow' onClick={handleClickLeft}> ← </button> 
 
+                    <div className='slides'>
+                        {
+                            slides.map((slides, i) => {
+                                return( 
+                                    <div className='slide-card'> This is a card with stuff in it
+                                    </div>
+                                
+                                )
+                            })
+                        }
+                    </div>
+                    <button className='forward-arrow' onClick={handleClickRight}> →  </button>
         </div>
 
     )
