34210a96567a48a2333fc894a42111482ac465c3
James Robertson <Jor4291@gmail.com>
2022-02-01 13:21:08 -0600

    added link buttons nav functionality to Header and footer

--- front-end/src/component/Footer.js
+++ front-end/src/component/Footer.js
@@ -1,22 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 
 
 const Footer = () => {
 
     return (
+        <div className="footer-section">
+            <div className="container">
+                <div className="row">
+                    <div className="nav">
+                    <Link className="home-button" to='/'>Anywhere Fitness</Link>
+                    <Link className="button" to='/classes'>Classes</Link>
+                    <Link className="button" to='/login'>Login</Link>
+                    <Link className="button" to='/register'>Register</Link>
+                    </div>
 
 
-
-
-
-
-
-<div className="copyright">
+    <div className="copyright">
 <p>
- Anywhere Fitness | Copyright 2022 |  | PrivacyTerms of Service
+  Copyright 2022 | Privacy | Terms of Service
 </p>
+         </div>
+      </div>
+    </div>
 </div>
 
     )
