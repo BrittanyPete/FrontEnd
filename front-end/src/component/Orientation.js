cd0d9a99f67c37bd8cf38a894c2e445a01164067
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-01 14:35:39 -0500

    finished most of the orientation page

--- front-end/src/component/Orientation.js
+++ front-end/src/component/Orientation.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Orientation.css';
-
 
 
 const Orientation = () => {
@@ -10,37 +10,42 @@ const Orientation = () => {
     // these are placeholder slides
     const slides = [1, 2, 3, 4, 5];
 
-
     const handleClickLeft = () => {
         
     }
 
-    const handleClickRight = () => {
-       
+    const handleClickRight = () => {  
 
     }
 
     return(
-
+    <div className='page-container'>
+        
         <div className='orientation-container'>
-            <div className='title'>Welcome to Anytime Fitness!</div>
+        <a href='http://localhost:3000/'>Skip tutorial</a>
+
+            <h2 className='title'>Welcome to Anytime Fitness!</h2>
+            
             <button className='back-arrow' onClick={handleClickLeft}> ← </button> 
 
                     <div className='slides'>
                         {
                             slides.map((slides, i) => {
-                                return( 
-                                    <div className='slide-card'> This is a card with stuff in it
-                                    </div>
-                                
+                                return( <div className='slide-card'>
+                                            <div className='card-stuff'> This is a card with stuff in it
+
+                                            <img src='https://images.unsplash.com/photo-1618265341355-d0e2d1fdf26b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60' alt='filler'/>
+                                            </div>
+                                        </div>
                                 )
                             })
                         }
                     </div>
                     <button className='forward-arrow' onClick={handleClickRight}> →  </button>
         </div>
-
+    </div>
     )
 }
+
 
 export default Orientation;
